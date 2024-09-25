d31ab75ff70e9f23cee720c466195d160b27a371
francis-robert <francis.robert@epa.gov>
2024-09-25 13:59:45 -0500

    Delete GMAPR2 directory

--- GMAPR2/R/MA_ST_bind.R
+++
@@ -1,7 +0,0 @@
-MA_ST_bind <- function(x,y){
-  output <- x %>%
-    bind_rows(., y) %>%
-    separate(name,c("type", "instrument", "residence_time_sec", "units", "header"), sep = "_") %>%
-    mutate(header = case_when (str_detect(instrument, c("Picarro|Syft")) ~ paste0("ANALYTE_",header),
-                               .default = header))
-}
--- GMAPR2/R/ts_plot.R
+++
@@ -1,5 +0,0 @@
-ts_plot <- function (x) {
-  data <- x %>%
-    mutate(header = gsub("ANALYTE_","", header))
-return(data)
-}
