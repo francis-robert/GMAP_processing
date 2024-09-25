0e96d82f32e95246d4589813d3f6406e91f4f570
francis-robert <francis.robert@epa.gov>
2024-09-25 13:57:48 -0500

    Delete GMAPR2 directory

--- GMAPR2/R/ts_table.R
+++
@@ -1,8 +0,0 @@
-ts_table <- function(x,rm_flagged = c("yes", "no")){
-  if(use_flag=="yes"){output <- x %>%
-    filter(.,str_detect(instrument, c("Picarro|Syft")))%>%
-    filter(mdl_flag == "NA" & time_flag == "NA")
-  }else{output <- x %>%
-      filter(.,str_detect(instrument, c("Picarro|Syft")))
-  }
-  }
