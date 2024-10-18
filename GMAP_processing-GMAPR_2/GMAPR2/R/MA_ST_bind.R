89fa3f4bee6590c6ff195e9a47694c4f4cc9caa5
francis-robert <francis.robert@epa.gov>
2024-10-18 09:48:58 -0500

    Delete GMAP_processing-GMAPR_2 directory

--- GMAP_processing-GMAPR_2/GMAPR2/R/MA_ST_bind.R
+++
@@ -1,9 +0,0 @@
-MA_ST_bind <- function(x,y){
-  output <- x %>%
-    bind_rows(., y) %>%
-    separate(name,c("type", "instrument", "residence_time_sec", "units", "header"), sep = "_") %>%
-    filter(!header == "Mode") %>%
-    mutate(value = as.numeric(value)) %>%
-    mutate(header = case_when (str_detect(instrument, c("Picarro|Syft")) ~ paste0("ANALYTE_",header),
-                               .default = header))
-}
