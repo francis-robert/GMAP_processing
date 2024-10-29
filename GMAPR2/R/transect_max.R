2848ebb448cc23b3b97cfe6c3f49dcd523472d8e
francis-robert <francis.robert@epa.gov>
2024-10-29 15:42:21 -0500

    10/29/2024 2nd updates
    added the updates from the work with Kate from 10/25/2024 into the main branch of these code.  In addition to the other commit that were added today which updated the issue when there is not a time in the data this update deals with focuses on the transect min/ max times, and the short and long transect analyte min/ max.

--- GMAPR2/R/transect_max.R
+++ GMAPR2/R/transect_max.R
@@ -1,13 +1,21 @@
 transect_max <- function (x){
-  output <- x %>%
+  output <- c()
+  output_short <- x %>%
     filter(.,str_detect(instrument, c("Picarro|Syft"))) %>%
     group_by(campaign, instrument, id, header) %>%
     slice_max(value, n=1) %>%
     slice_max(TimeStamp, n=1) %>%
     mutate(header = gsub("ANALYTE_","",header)) %>%
-    select("id","TimeStamp","header","value","time_flag") %>%
-    ungroup() %>%
-    pivot_wider(.,id_cols=id,names_from = header)
-  #write.csv(output,paste0(unique(output$campaign),"_transect_max_",unique(output$loc_samp),".csv"))
+    pivot_wider(.,id_cols = id,names_from = header)
+  output_long <- x %>%
+    filter(.,str_detect(instrument, c("Picarro|Syft"))) %>%
+    group_by(campaign, instrument, id, header) %>%
+    slice_max(value, n=1) %>%
+    slice_max(TimeStamp, n=1) %>%
+    mutate(header = gsub("ANALYTE_","",header))
+  output[["short"]] = output_short
+  output[["long"]] = output_long
+  write.csv(output[["short"]],paste0(unique(output_long$campaign),"_transect_max_",unique(output_long$loc_samp),".csv"))
+
   return(output)
 }
--- GMAPR2/R/transect_time_minmax.R
+++ GMAPR2/R/transect_time_minmax.R
@@ -1,7 +1,9 @@
 transect_time_minmax <-function (x){
   output <- x %>%
+    filter(TimeStamp > as.POSIXct("1900-01-01 00:00:01", format = "%Y-%m-%d %H:%M:%S"))%>%
     group_by(campaign, id) %>%
     summarise(min = min(TimeStamp),
               max = max(TimeStamp))
+
   return(output)
 }
