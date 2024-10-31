351a0f55a39a6b22c64ee1aede918ade6214c0b7
katehaile <haile.katherine@epa.gov>
2024-10-31 13:54:24 -0500

    Update time_flag.R
    10/31/24 update

--- GMAPR2/R/time_flag.R
+++ GMAPR2/R/time_flag.R
@@ -1,20 +1,32 @@
 time_flagging <- function(x, timestart = " ", timestop = " ", timeqt = " ",
-                      analyte = " ", procedure = " ") {
-  analyte_vec <- unique(analyte)
+                          analyte = " ", procedure = " ") {
+  analyte_vec <- paste0("ANALYTE_",unique(analyte))
   print(analyte_vec)
-
-  inter_df <- x %>%
-    mutate(if_else(str_detect(colnames(x),"time_flag") == TRUE, paste0(time_flag), time_flag == "NA"))
-
-  # df_change <- x %>%
-  #   filter(., str_detect(header, paste(analyte_vec, collapse="|"))) %>%
-  #   filter(TimeStamp >= timestart & TimeStamp <= timestop) %>%
-  #   mutate(time_flag = timeqt)
-  #     # mutate(paste0(analyte_vec[i],"_time_flag" = case_when(TimeStamp >= timestart & TimeStamp <= timestop & header == analyte_vec[i] ~ timeqt,
-  #     #                              .default = "NA"))
-  #
-  # df_no_change <- x %>%
-  #   filter(., !str_detect(header, paste(analyte_vec, collapse="|"))) %>%
-  #   filter(TimeStamp < timestart | TimeStamp > timestop) %>%
-  return(inter_df)
-  }
+  timestart <- as.POSIXct(timestart,tryFormats = c("%m/%d/%Y %H:%M:%S"))
+  print(timestart)
+  timestop <- as.POSIXct(timestop,tryFormats = c("%m/%d/%Y %H:%M:%S"))
+  
+  if (all(!str_detect(colnames(x),"time_flag"))){
+    inter_df <- x %>%
+      mutate(time_flag = "NA") %>%
+      mutate(rn = row_number())
+    print("Time Flagging Column Added")
+  }else{
+    inter_df <- x %>%
+      mutate(rn = row_number())
+    print("Time Flagging Column Already Present")}
+  df_change <- inter_df %>%
+    filter(., str_detect(header, paste(analyte_vec, collapse="|"))) %>%
+    filter(TimeStamp >= timestart & TimeStamp <= timestop) %>%
+    mutate(time_flag = paste(time_flag,timeqt,sep = ","))
+  
+  df_no_change <- subset(inter_df, !(rn %in% df_change$rn))
+  
+  
+  output <- df_change %>%
+    bind_rows(.,df_no_change) %>%
+    select(-rn)%>%
+    mutate(time_flag = gsub("NA,","",time_flag))
+  
+  return(output)
+}
